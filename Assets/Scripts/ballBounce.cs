4fe50ce50f2123efef03ce76192c2388ac0cb1ed
Arianne Gleiser <agleiser@usc.edu>
2023-02-20 11:12:35 -0800

    added screen shake

--- Assets/Scripts/ballBounce.cs
+++ Assets/Scripts/ballBounce.cs
@@ -6,6 +6,7 @@ public class ballBounce : MonoBehaviour
 {
     public AudioSource bounceSound;
     Rigidbody2D rb;
+    // public cameraShake cs;
     Vector3 lastVelocity;
 
     // Start is called before the first frame update
@@ -23,6 +24,7 @@ public class ballBounce : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         bounceSound.Play();
+        // cs.ShakeCamera();
         var speed = lastVelocity.magnitude;
         var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
         rb.velocity = direction * Mathf.Max(speed, 0f);
